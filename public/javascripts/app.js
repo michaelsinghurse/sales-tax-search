d7cdf37920530e6e55c143a02210c7c277efdb0c
Michael Singhurse <michaelsinghurse@gmail.com>
2020-07-13 17:30:04 -0500

    began refactoring - moving all functions into one object

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,115 +1,111 @@
 "use strict";
 
-const searchCounter = {
-  _count: 0,
-  get: function() {
-      return this._count;
+let App = {
+  bindListeners() {
+    $(".js-search-form").on("submit", this.handleFormSubmit.bind(this));
   },
-  increment: function() {
-      this._count += 1;
-  }
-};
+  
+  getSearchId: (function() {
+    let count = 0;
+    return function() {
+      count += 1;
+      return count;
+    };
+  })(),
 
-function handleAddressInput() {
-  $('.js-search-form').on('submit', function(event) {
-    event.preventDefault();
-
-    const street = $('#street').val().trim();
-    const city = $('#city').val().trim();
-    const state = $('#state').val().trim();
-    const zip = $('#zip').val().trim();
-
-    $('.js-search-form')[0].reset();
-
-    searchCounter.increment();
-
-    const searchObject = {
-      street: street,
-      city: city,
-      state: state,
-      zip: zip,
-      country: 'US',
-      searchId: searchCounter.get()
+  getSearchResultsFromServer(inputs) {
+    let settings = {
+      method: "GET",
+      url: "/api/rates",
+      data: inputs,
+      dataType: "html"
     };
 
-    getSearchResultsFromServer(searchObject);
-  });
-}
+    $.ajax(settings)
+      .done(data => {
+        this.renderHtmlFromServer(data);
+        this.instantiateAndHandleCopyButtons();
+        this.insertGoogleMapOfAddress(inputs); 
+      }) 
+      .fail((_jqXHR, textStatus) => {
+        let errorMessage = "<p>Request failed: Unable to retrieve rates from server. Please check the address and try again.</p>";
+        this.renderHtmlFromServer(errorMessage);
+      });
+  },
 
-function getSearchResultsFromServer(searchObject) {
-  const settings = {
-    method: 'GET',
-    url: '/api/rates',
-    data: searchObject,
-    dataType: 'html'
-  };
+  handleFormSubmit(event) {
+    event.preventDefault();
 
-  const request = $.ajax(settings);
+    let inputs = {
+      street: $('#street').val().trim(),
+      city: $('#city').val().trim(),
+      state: $('#state').val().trim(),
+      zip: $('#zip').val().trim(),
+      country: "US",
+      searchId: this.getSearchId()
+    };
 
-  request.done(function(data) {
-    renderHtmlFromServer(data);
-    instantiateAndHandleCopyButtons();
-    insertGoogleMapOfAddress(searchObject); 
-  }); 
-  request.fail(function(jqXHR, textStatus) {
-    const htmlErrorMessage = '<p>Request failed: Unable to retrieve rates from server. Please check the address and try again.</p>';
-    renderHtmlFromServer(htmlErrorMessage);
-  });
-}
+    event.currentTarget.reset();
 
-function renderHtmlFromServer(htmlString) {
-  const $resultsList = $('.js-results-list');
-  if ($resultsList.children().length === 0) {
-    $resultsList
-    .prop('hidden', false)
-    .html(htmlString);
-  }
-  else {
-    $resultsList.prepend(htmlString);
-  }
-}
+    this.getSearchResultsFromServer(inputs);
+  },
 
-function instantiateAndHandleCopyButtons() {
-  const clipboard = new Clipboard('.js-btn-copy-input');
-}
+  init() {
+    this.bindListeners();
+  },
 
-//first get latitude and longitude; then render the map
-function insertGoogleMapOfAddress(searchObject) {
-  const street = searchObject.street;
-  const city = searchObject.city;
-  const state = searchObject.state;
+  insertGoogleMapOfAddress(searchObject) {
+    const street = searchObject.street;
+    const city = searchObject.city;
+    const state = searchObject.state;
 
-  const geocoder = new google.maps.Geocoder();
-  const geocoderRequestObject = {
-    address: street + ', ' + city + ', ' + state,
-    componentRestrictions: {
-      country: searchObject.country,
-      postalCode: searchObject.zip
+    const geocoder = new google.maps.Geocoder();
+    const geocoderRequestObject = {
+      address: street + ', ' + city + ', ' + state,
+      componentRestrictions: {
+        country: searchObject.country,
+        postalCode: searchObject.zip
+      }
+    };
+
+    geocoder.geocode(geocoderRequestObject, (results, status) => {
+      if (status == 'OK') {
+        this.renderGoogleMapAtLatLng(results[0].geometry.location);
+      } else {
+        //console.log('Geocoder Error!');
+        $('#map').html('<p>Unable to load map for address given. Please check the address and try again.</p>');
+      }
+    });
+  },
+
+  instantiateAndHandleCopyButtons() {
+    const clipboard = new Clipboard(".js-btn-copy-input");
+  },
+
+  renderGoogleMapAtLatLng(addressLatLng) {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: addressLatLng,
+      zoom: 10
+    });
+
+    const marker = new google.maps.Marker({
+      position: addressLatLng,
+      map,
+      title: "Sales Location",
+    });
+  },
+
+  renderHtmlFromServer(htmlString) {
+    let $resultsList = $(".js-results-list");
+    if ($resultsList.children().length === 0) {
+      $resultsList.prop("hidden", false).html(htmlString);
     }
-  };
-
-  geocoder.geocode(geocoderRequestObject, function(results, status) {
-    if (status == 'OK') {
-      renderGoogleMapAtLatLng(results[0].geometry.location);
-    } else {
-      //console.log('Geocoder Error!');
-      $('#map').html('<p>Unable to load map for address given. Please check the address and try again.</p>');
+    else {
+      $resultsList.prepend(htmlString);
     }
-  });
-}
+  },
+};
 
-function renderGoogleMapAtLatLng(addressLatLng) {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: addressLatLng,
-    zoom: 10
-  });
-  const marker = new google.maps.Marker({
-    position: addressLatLng,
-    map: map,
-    title: 'Hello World'
-  });
-}
+$(App.init.bind(App));
 
-$(function() {
-  handleAddressInput();
-});
+
