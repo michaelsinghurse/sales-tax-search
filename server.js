37e17b65a7a41b68380729d62872ed1ad4bd0e9b
Michael Singhurse <michaelsinghurse@gmail.com>
2020-07-14 11:53:28 -0500

    removed unnecessary code

--- server.js
+++ server.js
@@ -7,49 +7,18 @@ const ratesRouter = require('./routes/ratesRouter');
 
 const app = express();
 
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("port", process.env.PORT || 8080);
+app.set("views", path.resolve(__dirname, 'views'));
+app.set("view engine", "ejs");
 
-app.use(morgan('common'));
-
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
-
-app.use('/api/rates', ratesRouter);
+app.use(morgan("common"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/rates", ratesRouter);
 
 app.use(function(req, res) {
   res.status(404).send("Page not found!");
 });
 
-let server;
-
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err);
-    });
-  });
-}
-
-function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('closing server now');
-    server.close(err => {
-      if(err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
-  });
-}
-
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-}
-
-module.exports = {app, runServer, closeServer};
+app.listen(app.get("port"), () => {
+  console.log(`app is listening on part ${app.get("port")}...`);
+});
